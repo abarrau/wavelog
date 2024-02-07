0bb1594bec45f119d380671d8ad5a87b628e44e9
abarrau <hesky378@gmail.com>
2024-02-07 14:07:46 +0100

    add js function

--- assets/js/leaflet/leafembed.js
+++ assets/js/leaflet/leafembed.js
@@ -3,8 +3,8 @@ var plotlayers=[];
 var iconsList={'qso':{'color':'#FF0000','icon':'fas fa-dot-circle'}};
 
 var stationIcon = L.divIcon({className:'cspot_station'});
-var qsoIcon = L.divIcon({className:'cspot_qso'}); //default (fas fa-dot-circle red)
-var qsoconfirmIcon = L.divIcon({className:'cspot_qsoconfirm'});
+var qsoIcon = L.divIcon({className:'cspot_qso cspot'}); //default (fas fa-dot-circle red)
+var qsoconfirmIcon = L.divIcon({className:'cspot_qsoconfirm cspot'});
 var redIconImg = L.icon({ iconUrl: icon_dot_url, iconSize: [10, 10] }); // old //
 
 var osmUrl = $('#leafembed').attr("tileUrl");
@@ -53,7 +53,7 @@ function initplot(_url_qso, options={}) {
 		} else {
 			$.ajax({
 				url: base_url+'index.php/user_options/get_map_custom', type: 'GET', dataType: 'json',
-				error: function() { askForPlots(_url_qso, options); console.log('[ERROR] ajax get_map_custom() function return error.'); },
+				error: function() { askForPlots(_url_qso, options); console.log('[ERROR][initplot()] ajax get_map_custom() function return error.'); },
 				success: function(json_mapinfo) {
 					if (typeof json_mapinfo.qso !== "undefined") { iconsList = json_mapinfo; }
 					if(json_mapinfo.gridsquare_show == "1") { maidenhead.addTo(map); }
@@ -72,7 +72,7 @@ function askForPlots(_url_qso, options={}) {
         error: function() { console.log('[ERROR] ajax askForPlots() function return error.'); },
         success: function(plotjson) {
         	if ((typeof plotjson['markers'] !== "undefined")&&(plotjson['markers'].length>0)) {
-				for (i=0;i<plotjson['markers'].length;i++) { createPlots(plotjson['markers'][i]); }
+				for (i=0;i<plotjson['markers'].length;i++) { createPlots(plotjson['markers'][i], options); }
         	}
         	if ((typeof plotjson['station']!=="undefined")&&(plotjson['station'].icon!="0")) {
         		createPlots(plotjson['station']);
@@ -84,16 +84,20 @@ function askForPlots(_url_qso, options={}) {
     });
 }
 
-function createPlots(_plot) {
+function createPlots(_plot, options={}) {
 	var plotll = new L.LatLng(_plot.lat,_plot.lng, true);
 	if (typeof _plot.icon==="undefined") { _plot.icon = 'qsoIcon'; }
 	if ((typeof iconsList.qsoconfirm!=="undefined")&&(typeof iconsList.qsoconfirm.icon!=="undefined")&&(iconsList.qsoconfirm.icon!="0")&&(typeof _plot.confirmed!=="undefined")&&(_plot.confirmed=='Y')) { _plot.icon = 'qsoconfirmIcon'; }
 	eval('var plotmark = new L.Marker(plotll, {icon: '+_plot.icon+' })');
-	plotmark.data=_plot;
+	//plotmark.data=_plot;
 	map.addLayer(plotmark);
 	if ((typeof _plot.label!=="undefined")&&(typeof _plot.html!=="undefined")) {
 		_plot.label = (_plot.label!="")?("<h3>"+_plot.label+"</h3>"):"";
 		if ((_plot.label+_plot.html)!="") { plotmark.bindPopup(_plot.label+_plot.html); }
+	}
+	// add qso id to plot //
+	if ((typeof _plot.qsoid !== "undefined") && (_plot.qsoid > 0)) {
+		$(options.map_id+' .cspot:last-child').attr('data-qsoid',_plot.qsoid);
 	}
 	plotlayers.push(plotmark);
 }
@@ -103,4 +107,25 @@ function removeMarkers() {
 		map.removeLayer(plotlayers[i]);
 	}
 	plotlayers=[];
+}
+
+// HighLight plot on MAP on hover QSO log //
+function setSyncLogWithMap(mapId='map', logTabClass='log-sync-map') {
+	$.ajax({
+		url: base_url+'index.php/user_options/get_map_custom', type: 'GET', dataType: 'json',
+		error: function() { console.log('[ERROR][setSyncLogWithMap()] ajax get_map_custom() function return error.'); },
+		success: function(json_mapinfo) {
+			if ((typeof json_mapinfo.highlightqso_show !== "undefined") && (json_mapinfo.highlightqso_show=="1")) {
+				$('.'+logTabClass+' .trqso').off('mouseenter').on('mouseenter', function () {
+					$('#'+mapId+' .cspot[data-qsoid="'+$(this).attr('id').replace('qso_','')+'"]').click();
+				});
+				$('.'+logTabClass+' .trqso').off('mouseleave').on('mouseleave', function () {
+					$('#'+mapId+' .cspot[data-qsoid="'+$(this).attr('id').replace('qso_','')+'"]').click();
+				});
+			}
+		}
+	});
+
+
+
 }
